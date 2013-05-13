9840fcd4ec6fc09449a738f10215e9afa84d3d14
Robert Williams <rob@r-williams.com>
2013-05-13 15:19:39 +0100

    Base response class

--- lib/simple_merchant/datacash/response.rb
+++ lib/simple_merchant/datacash/response.rb
@@ -1,6 +1,28 @@
 module SimpleMerchant
   module Datacash
     class Response
+
+      def initialize(response)
+        @response = response
+      end
+
+      # This states if the http request is successful
+      # not if a transaction is.
+      def successful?
+        @response.code == 200
+      end
+
+      def data
+        @response['Response']
+      end
+
+      def status
+        data['status'].to_i
+      end
+
+      def reference
+        data['datacash_reference']
+      end
     end
   end
 end
