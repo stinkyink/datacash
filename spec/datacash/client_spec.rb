9a8220e6418025190bcda5746b47508c53f8795c
Robert Williams <rob@r-williams.com>
2013-05-17 16:54:22 +0100

    removed query spec because it needs improving

--- spec/datacash/client_spec.rb
+++ spec/datacash/client_spec.rb
@@ -11,45 +11,4 @@ describe Datacash::Client do
   end
 
   let(:rest_client) { double("RestClient") }
-
-  describe "#query" do
-
-    subject { client.query(12345) }
-
-    context "with a good response" do
-
-      let(:response_xml) do
-        %Q{<?xml version="1.0" encoding="UTF-8"?>
-      <Response>
-        <datacash_reference>3600102439068417</datacash_reference>
-        <information></information>
-        <status>274</status>
-        <reason>I don't know why</reason>
-        <time>1368627820</time>
-      </Response>
-        }
-      end
-
-      before do
-        rest_client.stub(:post).and_return(response_xml)
-      end
-
-      it "should be a success" do
-        subject.should be_success
-      end
-    end
-
-    context "with a bad response" do
-      before do
-        rest_client.stub(:post).and_raise(RestClient::Exception)
-      end
-
-      it "should not be a success" do
-        subject.should_not be_success
-      end
-    end
-  end
-
-  describe "#request" do
-  end
 end
