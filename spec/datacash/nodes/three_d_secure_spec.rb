29815153f16382f1904579a9b7ce9d40c31409a8
Robert Williams <rob@r-williams.com>
2013-05-20 16:35:19 +0100

    Added warning to mock out Time

--- spec/datacash/nodes/three_d_secure_spec.rb
+++ spec/datacash/nodes/three_d_secure_spec.rb
@@ -13,5 +13,6 @@ describe Datacash::Nodes::ThreeDSecure do
   it "should have defaults" do
     subject[:verify].should eq('yes')
     subject[:purchase_desc].should eq('goods')
+    fail "Need to mock out purchase_datetime"
   end
 end
