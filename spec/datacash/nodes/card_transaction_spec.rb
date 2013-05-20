ba075945d7558e0cbce274adfc4fc6196c4db827
Robert Williams <rob@r-williams.com>
2013-05-20 16:23:39 +0100

    Added CardTransaction spec

---
+++ spec/datacash/nodes/card_transaction_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Datacash::Nodes::CardTransaction do
+
+  describe "#to_xml" do
+    subject { MultiXml.parse(described_class.new.to_xml) }
+
+    it "should have a root element of 'CardTxn'" do
+      subject.should have_key('CardTxn')
+    end
+  end
+
+  it "should have defaults" do
+    subject[:method].should eq('auth')
+  end
+
+  describe ":card" do
+    context "when set" do
+      before { subject[:card] = {} }
+
+      it "should be an Card instance" do
+        subject[:card].should be_kind_of(
+          Datacash::Nodes::Card
+        )
+      end
+    end
+  end
+end
