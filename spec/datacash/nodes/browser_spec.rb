5654ad61c699f6af6bbcd3874391921c6edaa42e
Robert Williams <rob@r-williams.com>
2013-05-20 16:31:02 +0100

    Added Browser spec

---
+++ spec/datacash/nodes/browser_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Datacash::Nodes::Browser do
+
+  describe "#to_xml" do
+    subject { MultiXml.parse(described_class.new.to_xml) }
+
+    it "should have a root element of 'CardTxn'" do
+      subject.should have_key('Browser')
+    end
+  end
+
+  it "should have defaults" do
+    subject[:device_category].should eq(0)
+    subject[:accept_headers].should eq('*/*')
+    subject[:user_agent].should eq('Mozilla/5.0')
+  end
+end
