87c46e085012016397da97a5409c944d9553eee1
Robert Williams <rob@r-williams.com>
2013-05-20 16:25:27 +0100

    Added HPSTransaction spec

---
+++ spec/datacash/nodes/hps_transaction_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Datacash::Nodes::HPSTransaction do
+
+  describe "#to_xml" do
+    subject { MultiXml.parse(described_class.new.to_xml) }
+
+    it "should have a root element of 'CardTxn'" do
+      subject.should have_key('HpsTxn')
+    end
+  end
+
+  it "should have defaults" do
+    subject[:method].should eq('setup_full')
+  end
+end
