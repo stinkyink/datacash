9e16635d1134851b92c23bf5af58e5a1e8146aa4
Robert Williams <rob@r-williams.com>
2013-05-20 16:28:31 +0100

    Added authentication_spec

---
+++ spec/datacash/nodes/authentication_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Datacash::Nodes::Authentication do
+
+  describe "#to_xml" do
+    subject { MultiXml.parse(described_class.new.to_xml) }
+
+    it "should have a root element of 'Authentication'" do
+      subject.should have_key('Authentication')
+    end
+  end
+end
