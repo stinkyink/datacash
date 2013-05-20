c07df3c24c797419d748f4e536cf741042b5a607
Robert Williams <rob@r-williams.com>
2013-05-20 16:50:44 +0100

    Added card spec.

---
+++ spec/datacash/nodes/card_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Datacash::Nodes::Card do
+
+  describe "#to_xml" do
+    subject { MultiXml.parse(described_class.new.to_xml) }
+
+    it "should have a root element of 'Card'" do
+      subject.should have_key('Card')
+    end
+  end
+
+  describe ":cv2avs" do
+    context "when set" do
+      before { subject[:cv2avs] = {} }
+
+      it "should be a Cv2Avs instance" do
+        subject[:cv2avs].should be_kind_of(
+          Datacash::Nodes::Cv2Avs
+        )
+      end
+    end
+  end
+end
