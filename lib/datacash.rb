ac627f466255a785c7cbf96dd4064d7da52316cf
Robert Williams <rob@r-williams.com>
2013-05-20 17:02:35 +0100

    Cleanup

--- lib/datacash.rb
+++ lib/datacash.rb
@@ -6,8 +6,13 @@ require 'date'
 require 'active_support/core_ext/hash/conversions'
 require 'hashie/extensions/structure'
 
+module Datacash
+  TIME_FORMAT = '%Y%m%d %H:%M:%S'
+end
+
 require 'datacash/nodes/node'
 require 'datacash/nodes/hps_transaction'
 require 'datacash/client'
 require 'datacash/response'
 require 'datacash/request'
+
