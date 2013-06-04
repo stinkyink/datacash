1b87d145ac267d5fd402aced238059952d6c4eb9
Rich Grundy <rich@27smiles.com>
2013-06-04 15:29:18 +0000

    Added redirect URL accessor.

--- lib/datacash/response/hps_transaction.rb
+++ lib/datacash/response/hps_transaction.rb
@@ -3,6 +3,10 @@ module Datacash
     class HpsTransaction < Base
       key :auth_attempts, from: 'AuthAttempts'
       coerce_key :auth_attempts, AuthAttempts
+
+      def redirect_url
+        "#{self[:hps_url]}?HPS_SessionID=#{self[:session_id]}"
+      end
     end
   end
 end
