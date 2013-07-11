f99d6044fa459b12e460e9f3a418777ba92db3bb
Robert Williams <rob@r-williams.com>
2013-07-11 10:49:14 +0100

    Fixed 3rd man key

--- spec/datacash/request/thirdman_spec.rb
+++ spec/datacash/request/thirdman_spec.rb
@@ -5,8 +5,8 @@ describe Datacash::Request::Thirdman do
   describe "#to_xml" do
     subject { MultiXml.parse(described_class.new.to_xml) }
 
-    it "should have a root element of 'Thirdman'" do
-      subject.should have_key('Thirdman')
+    it "should have a root element of 'The3rdMan'" do
+      subject.should have_key('The3rdMan')
     end
   end
 end
